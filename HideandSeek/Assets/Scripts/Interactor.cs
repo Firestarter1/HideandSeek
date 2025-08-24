316715386a5e5fda719c04df97a21781b6640b0a
MansaAlman <MSAlman@student.fullsail.edu>
2025-08-24 16:04:59 -0400

    Working on Interactable Objects
    Mapped "Interact" to e button. Created IInteractable interface inside of Interactor script.

---
+++ HideandSeek/Assets/Scripts/Interactor.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public interface IInteractable
+{
+    public void Interact();
+}
+
+public class Interactor : MonoBehaviour
+{
+    public Transform interactSource;
+    public float interactRange;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetButtonDown("Interact"))
+        {
+            Ray r = new Ray(interactSource.position, interactSource.forward);
+            if(Physics.Raycast(r, out RaycastHit hitInfo, interactRange))
+            {
+                if (hitInfo.collider.TryGetComponent(out IInteractable interactObj))
+                    interactObj.Interact();
+            }
+
+        }
+    }
+}
