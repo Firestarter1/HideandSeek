8b2814f52a88c606e1e0fe99456025ac9f7aec95
Firestarter1 <148384349+Firestarter1@users.noreply.github.com>
2025-08-25 15:34:37 -0400

    Added wallet to the Store and HealthPacks to store

--- HideandSeek/Assets/Scripts/Store.cs
+++ HideandSeek/Assets/Scripts/Store.cs
@@ -1,9 +1,36 @@
+using System.Xml.Serialization;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Store : MonoBehaviour, IInteractable
 {
+    public int wallet;
+    public int HealthPack;
+    public int Granade;
+    public int GunUpgrade;
+    public Text walletText;
+    public Text XPText;
+
+   
+
+    void Start()
+    {
+        wallet = 0;
+        walletText.text = wallet.ToString();
+    }
+
+  public void BuyHealth()
+    {
+        if(GameManager.Instance.playerScript.CheckFunds() > 50)
+        {
+            GameManager.Instance.playerScript.Heal(5);
+        }
+    }
+
     public void Interact()
     {
         GameManager.Instance.OpenStore();
     }
+
+   
 }
