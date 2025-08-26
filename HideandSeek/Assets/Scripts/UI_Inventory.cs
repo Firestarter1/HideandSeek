8fe8ee78fa8848fe93fc5daa94d9bad4c7b05d0f
MansaAlman <MSAlman@student.fullsail.edu>
2025-08-25 21:33:44 -0400

    Implemented Draggable Items for Inventory
    Created draggable item script, updated the inventory UI, created item slot prefab and script.

---
+++ HideandSeek/Assets/Scripts/UI_Inventory.cs
@@ -0,0 +1,11 @@
+using UnityEngine;
+
+public class UI_Inventory : MonoBehaviour
+{
+    private Inventory inventory;
+
+    public void SetInventory(Inventory inventory)
+    {
+        this.inventory = inventory;
+    }
+}
